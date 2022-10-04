35d240f5b4a017cc0190cb11c41418946aa26f3f
Ayaan <93127341+LaborotoryCode@users.noreply.github.com>
2022-10-04 12:21:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import keyboard
 import random
-import Offense
-import Defense
-from Offense import *
+def attack(attackIndex, receiverDefence, receiverHealth, attackerStamina):
+    print("akhdsgfjsdhvflkahsdf"+str(int(attackIndex*10*receiverDefence)))
+    receiverHealth -= int(attackIndex*10*receiverDefence)
+    attackerStamina -= attackIndex*10
+
+
+# import Defence
 
 HP_Player = 100
 HP_Computer = 100
 Stamina_Player = 50
 Stamina_Computer = 50
-Choice_Player = 1
-Choice_Computer = 1
+Choice_Player = 0
+Choice_Computer = 0
 round = 0
 
 #First we give the player the game instructions:
@@ -17,160 +20,55 @@ print("Welcome to our textfighter\nHere are a few instructions to get you starte
 
 print(round)
 
+#Get player's choice
+def PlayersTurn():
+    Choice_Player = input(print("Choose an option between 1-6 by typing that number: "))
+    while not (Choice_Player.isdigit() and 1<= int(Choice_Player) <=6):
+        Choice_Player = input("Invalid choice, please re-enter: ")
+    #cast after the choice is correct
+    Choice_Player = int(Choice_Player)
+
 #Get computer choice and make sure they have the stamina
-def computer_turn() :
-    global computer_choice
-    if Stamina_Computer >= 40:
-        computer_choice = random.randint(1,6)
-    elif Stamina_Computer >= 30:
-        while computer_choice == 4:
-            random.randint(1,6)
-    elif Stamina_Computer >= 20:
-        while computer_choice == 4 or 3:
-            random.randint(1,6)
-    elif Stamina_Computer >= 10:
-        while computer_choice == 4 or 3 or 2:
-            random.randint(1,6)
+def ComputerTurn(computerStamina): #need to fix this up
+    if computerStamina >= 40:
+        return random.randint(1,4) if random.randint(1,2) == 1 else random.randint(4,6)
+    elif computerStamina >= 30:
+        return random.randint(1,4) if random.randint(1,2) == 1 else random.randint(4,6)
+    elif computerStamina >= 20:
+        return random.randint(1,4) if random.randint(1,2) == 1 else random.randint(4,6)
+    elif computerStamina >= 10:
+        return random.randint(1,4) if random.randint(1,2) == 1 else random.randint(4,6)
     else:
-        while computer_choice == 4 or 3 or 2 or 1:
-            random.randint(1,6)
+        return random.randint(5,6)
 
-    
-#Get player choices
-if keyboard.is_pressed("1"):
-    Choice_Player = 1
-elif keyboard.is_pressed("2"):
-    Choice_Player = 2
-elif keyboard.is_pressed("3"):
-    Choice_Player = 3
-elif keyboard.is_pressed("4"):
-    Choice_Player = 4
-elif keyboard.is_pressed("5"):
-    Choice_Player = 5
-elif keyboard.is_pressed("6"):
-    Choice_Player = 6
 
+def getDefence(choice, stamina):
+    stamina += choice*10
+    return 0.2 if choice == 1 else 0.1
 
 #We can make them fight by calling this function
-def Fight() :
-    if Choice_Player == 1:
-        if Stamina_Player < 10:
-            attack1
-        else: 
-            round -= 1
-            return("You do not have enough stamina")
-        if computer_choice == 1:
-            attack1(1, HP_Player, Stamina_Player)
-        elif computer_choice == 2:
-            attack2(1, HP_Player, Stamina_Player)
-        elif computer_choice == 3:
-            attack3(1, HP_Player, Stamina_Player)
-        elif computer_choice == 4:
-            attack4(1, HP_Player, Stamina_Player)
-        elif computer_choice == 5:
-            defence1()
-        elif computer_choice == 6:
-            defence2
-    elif Choice_Player == 2:
-        if Stamina_Player < 20:
-            attack2
-        else: 
-            round -= 1
-            return("You do not have enough stamina")
-        if computer_choice == 1:
-            attack1(1, HP_Player, Stamina_Player)
-        elif computer_choice == 2:
-            attack2(1, HP_Player, Stamina_Player)
-        elif computer_choice == 3:
-            attack3(1, HP_Player, Stamina_Player)
-        elif computer_choice == 4:
-            attack4(1, HP_Player, Stamina_Player)
-        elif computer_choice == 5:
-            defence1
-        elif computer_choice == 6:
-            defence2
-    elif Choice_Player == 3:
-        if Stamina_Player < 30:
-            attack3
-        else: 
-            round -= 1
-            return("You do not have enough stamina")
-        if computer_choice == 1:
-            attack1(1, HP_Player, Stamina_Player)
-        elif computer_choice == 2:
-            attack2(1, HP_Player, Stamina_Player)
-        elif computer_choice == 3:
-            attack3(1, HP_Player, Stamina_Player)
-        elif computer_choice == 4:
-            attack4(1, HP_Player, Stamina_Player)
-        elif computer_choice == 5:
-            defence1
-        elif computer_choice == 6:
-            defence2
-    elif Choice_Player == 4:
-        if Stamina_Player < 40:
-            attack4
-        else: 
-            round -= 1
-            return("You do not have enough stamina")
-        if computer_choice == 1:
-            attack1(1, HP_Player, Stamina_Player)
-        elif computer_choice == 2:
-            attack2(1, HP_Player, Stamina_Player)
-        elif computer_choice == 3:
-            attack3(1, HP_Player, Stamina_Player)
-        elif computer_choice == 4:
-            attack4(1, HP_Player, Stamina_Player)
-        elif computer_choice == 5:
-            defence1
-        elif computer_choice == 6:
-            defence2
-    elif Choice_Player == 5:
-        defence1
-        if computer_choice == 1:
-            attack1(1, HP_Player, Stamina_Player)
-        elif computer_choice == 2:
-            attack2(1, HP_Player, Stamina_Player)
-        elif computer_choice == 3:
-            attack3(1, HP_Player, Stamina_Player)
-        elif computer_choice == 4:
-            attack4(1, HP_Player, Stamina_Player)
-        elif computer_choice == 5:
-            defence1
-        elif computer_choice == 6:
-            defence2
-    elif Choice_Player == 1:
-        defence2
-        if computer_choice == 1:
-            attack1(1, HP_Player, Stamina_Player)
-        elif computer_choice == 2:
-            attack2(1, HP_Player, Stamina_Player)
-        elif computer_choice == 3:
-            attack3(1, HP_Player, Stamina_Player)
-        elif computer_choice == 4:
-            attack4(1, HP_Player, Stamina_Player)
-        elif computer_choice == 5:
-            defence1
-        elif computer_choice == 6:
-            defence2
+def Fight(playerChoice, computerChoice, round):
+    #ADD STAMINA
+    playerDefence = getDefence(playerChoice, Stamina_Player) if playerChoice >= 5 else 1
+    computerDefence = getDefence(computerChoice, Stamina_Computer) if computerChoice >= 5 else 1
+    
+    if playerDefence == 0:
+        attack(playerChoice, computerDefence, HP_Computer, Stamina_Player)
+    if computerDefence == 0:
+        attack(computerChoice, playerDefence, HP_Player, Stamina_Computer)
     round += 1
 
 #Now we call the fight function so long as there are still rounds left
 while round <= 30 and HP_Player > 0 and HP_Computer > 0:
-    Fight()
-    print("Your health is: {HP_Player}/100")
-    print("Your stamina is: {Stamina_Player}/50")
-    print("This is round {round}/30")
+    Choice_Computer = ComputerTurn(Stamina_Computer)
+    PlayersTurn()
+    Fight(Choice_Player, Choice_Computer, round)
+    print("The computer chose:", Choice_Computer)
+    print("Your health is: "+ str(HP_Player) + "/100")
+    print("Your stamina is: " + str(Stamina_Player) + "/50")
+    print("This is round: " + str(round) + "/30")
 
 if HP_Player > 0:
     print("Congratulations you have won!")
 elif HP_Computer > 0:
     print("Unfortunately you have lost")
-
-
-
-
-
-
-
-
